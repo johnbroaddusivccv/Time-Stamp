7903c91cc7cdb050b7ec6f7c1ac2ee91391fd66b
Thomas Raya <Thomas.Raya@tmhp.com>
2019-04-13 12:28:08 -0500

    added firebase CDN to index.html
    created onclick event handler for new employee entries
    
    defualt values now display on firebase DB

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,20 @@ var config = {
   var eMonthlyRate = 0;
   var eTotalBilled = 0;
 
+$("#submitBtn").on("click", function () {
+    eName = $("#name-input").val().trim();
+    eRole = $("#destination-input").val().trim();
+    eStartDate = $("#ftt-input").val().trim();
+    eMonthlyRate = $("frequency-input").val().trim();
 
+    console.log(eName);
 
-  database.ref.set({
-      Employee: eName,
-      Role: eRole,
-      Date: eStartDate,
-      MonthsWorked: eMonthsWorked,
-      MonthlyRate: eMonthlyRate,
-      TotalBilled: eTotalBilled
-  })
+    database.ref.set({
+        Employee: eName,
+        Role: eRole,
+        Date: eStartDate,
+        MonthsWorked: eMonthsWorked,
+        MonthlyRate: eMonthlyRate,
+        TotalBilled: eTotalBilled
+    })
+})
